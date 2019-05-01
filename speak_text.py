30a610e25b9cb803d3691bde388c398d5d65d1e1
Aoesis <aoesis@hotmail.com>
2019-04-30 21:01:45 -0500

    Update speak_text.py

--- speak_text.py
+++ speak_text.py
@@ -5,9 +5,7 @@ cmd_beg='espeak -s100 '
 cmd_end='| aplay /home/pi/Desktop/Text.wav 2>/dev/null'
 cmd_out= '--stdout > /home/pi/Desktop/Text.wav'
 
-##text = raw_input('enter the text: ')
-##print (text)
 
-text = 'Paracetamol'
+text = 'texto'
 
 call([cmd_beg+cmd_out+' ' +text+cmd_end], shell=True)
